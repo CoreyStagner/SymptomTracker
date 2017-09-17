1745c063e63cdb08a0d984829264a07c1fab8843
Corey R. Stagner <cresc0428@gmail.com>
2017-09-17 12:25:42 -0400

    Added start of schema and seeds file

---
+++ db/schema.sql
@@ -0,0 +1,17 @@
+-- Create patients table
+
+CREATE TABLE patients (
+	id INT,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	phone_number VARCHAR(50)
+);
+
+-- Create doctor table
+
+CREATE TABLE doctor (
+	id INT,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	phone_number VARCHAR(50)
+);
---
+++ models/doctor.js
@@ -0,0 +1,52 @@
+console.log("Opened file [./models/doctor.js]");
+
+module.exports = function(sequelize, DataTypes) {
+  var doctor = sequelize.define("doctor", {
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: {
+          args: [1,50],
+          msg: "Please enter your first name"
+        }// end len
+      }//end validate
+    },// end first_name
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: {
+          args: [1,50],
+          msg: "Please enter your last name"
+        }// end len
+      }// end validate
+    },// end last_name
+    phone_number:{
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: {
+          args: 10,
+          msg: "Please enter a 10 digit phone number with no parenthesis ex. 5551234567"
+        }// end len
+      }// end validate
+    }// end phone_number
+  });// end define("doctor")
+
+  doctor.associate = function(models) {
+    // Associating doctor with doctor
+    doctor.hasMany(models.patient, {
+
+    });// end hasMany()
+  };// end doctor.associate
+
+  // doctor.associate = function(models) {
+  //   // Associating doctor with specialty
+  //   doctor.hasMany(models.specialty, {
+
+  //   });// end hasMany()
+  // };// end doctor.associate
+  
+  return doctor;
+};// end module.exports
