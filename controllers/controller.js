046a50ee90dde888019e7e98f871717d2733a59d
Corey R. Stagner <cresc0428@gmail.com>
2017-09-19 18:20:07 -0400

    Added pages for doctor, patient, and symptom

--- controllers/controller.js
+++ controllers/controller.js
@@ -6,6 +6,10 @@ var router = express.Router();
 var model = require('../models');
 
 module.exports = function(app){
+  app.get("/", function(req, res){
+    res.render("index");
+  });// end app.get("/patients");
+
   app.get("/patients", function(req, res){
     model.patients.findAll({}).then(function(data) {
       console.log(data);
